2230e03cba56deed790afea2b33b317e7f8db306
Ida Berg <ida.berg2003@gmail.com>
2025-05-21 16:46:37 +0200

    skapat login & register page

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main>
+      <div class="login-section">
+        <h1>Login</h1>
+        <form>
+          <div class="input-group">
+            <label htmlFor="">Email Address</label>
+            <input type="email" placeholder="Enter your email address" />
+          </div>
+          <div class="input-group">
+            <label htmlFor="">Password</label>
+            <input type="text" placeholder="Enter your password" />
+          </div>
+          <Link class="login-btn" type="submit" to={`/`}>Log In</Link>
+        </form>
+        <p class="text">
+          Don't have an account? 
+          <Link class="signUp" to={`/register`}> Sign up</Link>
+        </p>
+      </div>
+    </main>
   )
 }
 
